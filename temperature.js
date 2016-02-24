5e566b52618b31c0ce7b60c9e998b0d08b22a8e5
marrero- <alexmd54@gmail.com>
2016-02-24 17:16:55 +0000

    Terminado el html y css.  Refactorización del fichero temperature.js para evitar que escriba undefined en el caso  de que el matching haya fallado.  La RegExp tenía un bug porque aceptaba cadenas sin letra inicial.  Todo :ok_hand: .

--- temperature.js
+++ temperature.js
@@ -2,7 +2,7 @@
 function calculate() {
   var result;
   var temp = original.value;
-  var regexp = /([-+]?\d+(?:\.\d*)?)\s*((?:e[-]?\d+)?)\s*(c?e?l?s?i?u?s?f?h?a?r?e?n?h?e?i?t?)$/i;
+  var regexp = /([-+]?\d+(?:\.\d*)?)\s*((?:e[-]?\d+)?)\s*(ce?l?s?i?u?s?||fa?h?r?e?n?h?e?i?t?)$/i;
   var m = temp.match(regexp);
   if (m) {
     var num = m[1];
@@ -13,19 +13,26 @@ function calculate() {
     exp = parseInt(exp);
     if(isNaN(exp))
       exp = 1;
-    if (type == 'c' || type == 'ce' || type == 'cel' || type == 'cels'|| type == 'celsi' || type == 'celsiu' || type == 'celsius') {
-      result = (Math.pow(num,exp) * 9 / 5) + 32;
-      result = result.toFixed(1) + " Fharenheit"
+    if (type == 'c' || type == 'ce' ||
+        type == 'cel' || type == 'cels'||
+        type == 'celsi' || type == 'celsiu' ||
+        type == 'celsius') {
+            result = (Math.pow(num,exp) * 9 / 5) + 32;
+            result = result.toFixed(1) + " Fharenheit"
+            converted.innerHTML = result;
     }
     else {
-      if (type == 'f' ||  type == 'fh'|| type == 'fha' || type == 'fhar' || type == 'fhare'|| type == 'fharen' || type == 'fharenh' || type == 'fharenhe' ||  type == 'fharenhei' ||  type == 'fharenheit') {
-      result = (Math.pow(num,exp) - 32) * 5 / 9;
-      result = result.toFixed(1) + " Celsius"
+      if (type == 'f' ||  type == 'fa'|| type == 'fah' ||
+        type == 'fahr' || type == 'fahre'|| type == 'fahren' ||
+        type == 'fahrenh' || type == 'fahrenhe' ||  type == 'fahrenhei' ||
+        type == 'fahrenheit') {
+            result = (Math.pow(num,exp) - 32) * 5 / 9;
+            result = result.toFixed(1) + " Celsius"
+            converted.innerHTML = result;
+        }
+        else {
+          converted.innerHTML = "ERROR! Try something like '-4.2C' instead";
+        }
     }
-    }
-    converted.innerHTML = result;
-  }
-  else {
-    converted.innerHTML = "ERROR! Try something like '-4.2C' instead";
   }
 }

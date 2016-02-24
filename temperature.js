2541d69293abb6f3937ada40786989c915719e46
Gabriel Melian Hernandez <alu0100819786@ull.edu.es>
2016-02-24 16:32:46 +0000

    Cuarta Modificación Expresión Regular
    -Cambios en la expresión regular para que acepte todos los prefijos de
    ambas temperaturas y que reconozca todos los caracteres como minúscula

--- temperature.js
+++ temperature.js
@@ -2,23 +2,23 @@
 function calculate() {
   var result;
   var temp = original.value;
-  var regexp = /([-+]?\d+(?:\.\d*)?)\s*((?:e[-]?\d+)?)\s*(f(a||ar)||c(e||el))$/i;
+  var regexp = /([-+]?\d+(?:\.\d*)?)\s*((?:e[-]?\d+)?)\s*(c?e?l?s?i?u?s?f?h?a?r?e?n?h?e?i?t?)$/i;
   var m = temp.match(regexp);
   if (m) {
     var num = m[1];
     var exp = m[2];
-    var type = m[3];
+    var type = m[3].toLowerCase();
     num = parseFloat(num);
     exp = exp.replace('e','');
     exp = parseInt(exp);
     if(isNaN(exp))
       exp = 1;
-    if (type == 'c' || type == 'C' || type == 'ce' || type == 'cel') {
+    if (type == 'c' || type == 'ce' || type == 'cel' || type == 'cels'|| type == 'celsi' || type == 'celsiu' || type == 'celsius') {
       result = (Math.pow(num,exp) * 9 / 5) + 32;
-      result = result.toFixed(1) + " Farenheit"
+      result = result.toFixed(1) + " Fharenheit"
     }
     else {
-      if (type == 'f' || type == 'F' || type == 'fa' || type == 'far') {
+      if (type == 'f' ||  type == 'fh'|| type == 'fha' || type == 'fhar' || type == 'fhare'|| type == 'fharen' || type == 'fharenh' || type == 'fharenhe' ||  type == 'fharenhei' ||  type == 'fharenheit') {
       result = (Math.pow(num,exp) - 32) * 5 / 9;
       result = result.toFixed(1) + " Celsius"
     }
